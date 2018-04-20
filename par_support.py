d0e9f44c1235ba82e5b350b063e34116f7288be7
ChrisJFarr <chris.farr@cerner.com>
2018-04-20 17:06:59 -0500

    Combined entire dataset
    Memory problems with PCA
    Too slow of execution for forward stepwise selection
    Moving to answer specific questions, see working_eda.py top notes at this checkpoint for next step.

--- par_support.py
+++ par_support.py
@@ -1,23 +1,42 @@
 import numpy as np
+import pandas as pd
+
+"""
+start = time()
+results_par = Parallel(n_jobs=7)(delayed(par_support.par_transformations)(cont_vars_df.loc[:, [feat]].copy()) for feat in cont_vars_df.columns)
+x_data = pd.concat(results_par[0:2])
+for df in results_par[2:]:
+    x_data = pd.concat([x_data, df])
+stop = time()
+print(str(stop - start) + " seconds")
+"""
 
 
-def par_transformations(data, feat):
-    data = data.loc[:, feat].copy()
-    if data.min() > 0:  # Avoid 0 or negative
-        data.loc[:, feat + "_log"] = data[feat].apply(np.log)  # log
-        data.loc[:, feat + "_log2"] = data[feat].apply(np.log2)  # log2
-        data.loc[:, feat + "_log10"] = data[feat].apply(np.log10)  # log10
-    data.loc[:, feat + "_cubert"] = data[feat].apply(
+def par_transformations(data):
+    """
+    Input single featuer dataframe, return feature with added transformations
+    :param data:
+    :param feat:
+    :return:
+    """
+    assert isinstance(data, pd.DataFrame), "Input must be a dataframe"
+    feature_df = data.iloc[:, 0].copy()
+    feat = data.columns[0]
+    if feature_df.min() > 0:  # Avoid 0 or negative
+        data.loc[:, feat + "_log"] = feature_df.apply(np.log)  # log
+        data.loc[:, feat + "_log2"] = feature_df.apply(np.log2)  # log2
+        data.loc[:, feat + "_log10"] = feature_df.apply(np.log10)  # log10
+    data.loc[:, feat + "_cubert"] = feature_df.apply(
         lambda x: np.power(x, 1 / 3))  # cube root
-    data.loc[:, feat + "_sqrt"] = data[feat].apply(np.sqrt)  # square root
+    data.loc[:, feat + "_sqrt"] = feature_df.apply(np.sqrt)  # square root
     # Avoid extremely large values, keep around 1M max
-    if data.max() < 13:
-        data.loc[:, feat + "_exp"] = data[feat].apply(np.exp)  # exp
-    if data.max() < 20:
-        data.loc[:, feat + "_exp2"] = data[feat].apply(np.exp2)  # exp2
-    if data.max() < 100:
-        data.loc[:, feat + "_cube"] = data[feat].apply(
+    if feature_df.max() < 13:
+        data.loc[:, feat + "_exp"] = feature_df.apply(np.exp)  # exp
+    if feature_df.max() < 20:
+        data.loc[:, feat + "_exp2"] = feature_df.apply(np.exp2)  # exp2
+    if feature_df.max() < 100:
+        data.loc[:, feat + "_cube"] = feature_df.apply(
             lambda x: np.power(x, 3))  # cube
-    if data.max() < 1000:
-        data.loc[:, feat + "_sq"] = data[feat].apply(np.square)  # square
+    if feature_df.max() < 1000:
+        data.loc[:, feat + "_sq"] = feature_df.apply(np.square)  # square
     return data
--- working_eda.py
+++ working_eda.py
@@ -35,6 +35,24 @@ Scaling the feature and creating a new target that is an average of the potency 
 
 # How well can we predict the performance of the target without using it to train?
 
+* Can we augment the data set with predicted negative data (molecules expected to be inactive) to improve our machine
+learning models? TODO Next step here
+
+Dimension reduction techniques
+1. Load and combine entire dataset including decoys
+* Tried PCA with .25% dim len, computer crashed
+* Can't perform transformations
+* Try nnet to predict target OSM-S-106?
+3. Extract feature importance (try different methods)
+4. Plot to show potent, non-potent, un-tested, and decoys with top negative dimensions
+
+# Take features selected above and entire dataset
+# Train multiple models: hold out the OCM-S-106 as the test set
+# Assume all decoys have 100 IC50
+
+Notes:
+    * Are there features dropped due to adding decoys that prove as predictors in methods without decoys?
+# Try same methods with transforming the IC50 by the method described in.. need to find the paper/website again
 """
 import pandas as pd
 from sklearn.decomposition import KernelPCA
@@ -45,115 +63,12 @@ from scipy.stats import skewtest
 import os
 import pandas as pd
 import gc
-
-# Read in dataset
-df = pd.read_csv("data/Series3_6.15.17_padel.csv")
-
-""" Preprocessing Variables """
-# Get dummy vars: filter to int type, convert to object, pass to get_dummies.
-cat_vars_df = pd.get_dummies(
-    df[df.columns[df.iloc[:, :].dtypes == 'int64']].astype('O'))
-
-# Impute or remove? (for now remove any columns with nan)
-cont_vars_df = df[df.columns[df.iloc[:, :].dtypes == 'float64']].dropna(axis=1)
-# Note: Target variable is dropped from both dataframes and only in df
-
-
-def add_transformations(data, feat):
-    feature_df = data.loc[:, feat].copy()
-    if feature_df.min() > 0:  # Avoid 0 or negative
-        data.loc[:, feat + "_log"] = feature_df.apply(np.log)  # log
-        data.loc[:, feat + "_log2"] = feature_df.apply(np.log2)  # log2
-        data.loc[:, feat + "_log10"] = feature_df.apply(np.log10)  # log10
-    data.loc[:, feat + "_cubert"] = feature_df.apply(
-        lambda x: np.power(x, 1 / 3))  # cube root
-    data.loc[:, feat + "_sqrt"] = feature_df.apply(np.sqrt)  # square root
-    # Avoid extremely large values, keep around 1M max
-    if feature_df.max() < 13:
-        data.loc[:, feat + "_exp"] = feature_df.apply(np.exp)  # exp
-    if feature_df.max() < 20:
-        data.loc[:, feat + "_exp2"] = feature_df.apply(np.exp2)  # exp2
-    if feature_df.max() < 100:
-        data.loc[:, feat + "_cube"] = feature_df.apply(
-            lambda x: np.power(x, 3))  # cube
-    if feature_df.max() < 1000:
-        data.loc[:, feat + "_sq"] = feature_df.apply(np.square)  # square
-    return data
-
-
-# Add above transformations for all continuous variables
-for feature in cont_vars_df.columns:
-    cont_vars_df = add_transformations(cont_vars_df, feature)
-# Drop any new columns with NaN due to improper transformation
-cont_vars_df.replace([np.inf, -np.inf], np.nan, inplace=True)
-cont_vars_df.dropna(axis=1, inplace=True)
-
-# Assume skewed if we can reject the null hypothesis with 95% certainty
-# Remove any skewed features after adding transformations
-cont_vars_df = cont_vars_df.loc[:, cont_vars_df.apply(
-    lambda x: skewtest(x)[1] > .05).values]
-
-# Combine datasets
-x_data = pd.concat([cat_vars_df, cont_vars_df], axis=1)
-y_data = df.IC50
-
-# Remove missing IC50
-x_no_ic50 = x_data.loc[df.IC50.isnull(), :]
-x_no_ic50_names = df.loc[df.IC50.isnull(), "Name"]
-
-# Perform forward stepwise feature selection
-x_data = x_data.loc[~df.IC50.isnull(), :].reset_index(drop=True)
-y_data = y_data.loc[~df.IC50.isnull()].reset_index(drop=True)
-
-# Select features that are the best at predicting OSM-S-106
-
-
-# Loop through each column, train on all but 0, test on 0
-cont_vars = x_data.columns[x_data.iloc[:, :].dtypes == 'float64']
-
-# Train model
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.metrics import mean_squared_error
-
-
-best_feat = None
-best_score = np.inf
-# TODO Start here! Working through step-wise feature selection
-# TODO continue loop by adding to list of features and tracking what haven't been added yet
-for feat in cont_vars[0:100]:
-    feat = best_feat
-    x_train, y_train = x_data.loc[1:, feat], y_data.loc[1:]
-    x_test, y_test = x_data.loc[0, feat], y_data.loc[0]
-
-    # Scale
-    x_scaler, y_scaler = StandardScaler(), MinMaxScaler(feature_range=(.01, .99))
-    x_train = x_scaler.fit_transform(x_train.values.reshape(-1, 1))
-    x_test = x_scaler.transform(x_test)
-    y_train = y_scaler.fit_transform(y_train.values.reshape(-1, 1))
-    y_test = y_scaler.transform(y_test)
-
-    # Model
-    model = MLPRegressor(hidden_layer_sizes=(1,), activation="logistic",
-                         max_iter=1000, learning_rate="adaptive")
-    model.fit(x_train, y_train.ravel())
-    pred = model.predict(x_test)
-    # train_pred = model.predict(x_train)
-
-    mse = mean_squared_error(y_scaler.inverse_transform(y_test.reshape(-1, 1)),
-                             y_scaler.inverse_transform(pred.reshape(-1, 1)))
-    rmse = np.sqrt(mse)
-
-    if rmse < best_score:
-        best_feat = feat
-        best_score = rmse
-
-
-
-
-
-
-
+from par_support import par_transformations
+from joblib import Parallel, delayed
+from time import time
+import par_support
+from importlib import reload
+reload(par_support)
 """ Load and combine all datasets """
 file_list = os.listdir("data")
 file_list = [f for f in file_list if "decoys" in f and f.endswith(".csv")]
@@ -161,7 +76,7 @@ file_list = [f for f in file_list if "decoys" in f and f.endswith(".csv")]
 df = pd.read_csv("data/" + file_list[0])
 for f in file_list[1:]:
     df = df.append(pd.read_csv("data/" + f))
-
+df["IC50"] = 100
 # Combine with Series3_6.15.17_padel.csv
 tests = pd.read_csv("data/Series3_6.15.17_padel.csv")
 tests.IC50.fillna(-1, inplace=True)  # Mark potential compounds with -1
@@ -172,49 +87,15 @@ x_data = df.dropna(axis=1)
 del df
 gc.collect()
 
-# Primarily floats have complete features across datasets
-print("%s continuous features" % sum(x_data.dtypes == "float64"))
-print("%s categorical features" % sum(x_data.dtypes == "int64"))
 
-""" Preprocessing """
+""" Preprocessing Variables """
+# Get dummy vars: filter to int type, convert to object, pass to get_dummies.
+assert not sum(x_data[x_data.columns[x_data.iloc[:, :].dtypes == 'int64']].isna().sum()), "Null values found in cat"
+cat_vars_df = pd.get_dummies(
+    x_data[x_data.columns[x_data.iloc[:, :].dtypes == 'int64']].astype('O'))
 
-""" Process Continuous Variable """
-
-
-def add_transformations(data, feat):
-    feature_df = data.loc[:, feat].copy()
-    if feature_df.min() > 0:  # Avoid 0 or negative
-        data.loc[:, feat + "_log"] = feature_df.apply(np.log)  # log
-        data.loc[:, feat + "_log2"] = feature_df.apply(np.log2)  # log2
-        data.loc[:, feat + "_log10"] = feature_df.apply(np.log10)  # log10
-    data.loc[:, feat + "_cubert"] = feature_df.apply(
-        lambda x: np.power(x, 1 / 3))  # cube root
-    data.loc[:, feat + "_sqrt"] = feature_df.apply(np.sqrt)  # square root
-    # Avoid extremely large values, keep around 1M max
-    if feature_df.max() < 13:
-        data.loc[:, feat + "_exp"] = feature_df.apply(np.exp)  # exp
-    if feature_df.max() < 20:
-        data.loc[:, feat + "_exp2"] = feature_df.apply(np.exp2)  # exp2
-    if feature_df.max() < 100:
-        data.loc[:, feat + "_cube"] = feature_df.apply(
-            lambda x: np.power(x, 3))  # cube
-    if feature_df.max() < 1000:
-        data.loc[:, feat + "_sq"] = feature_df.apply(np.square)  # square
-    return data
-
-# Add above transformations for all continuous variables
-for feature in x_data.columns:
-    if x_data.loc[:, feature].dtype == "float64":
-        x_data = add_transformations(x_data, feature)
-
-# Parallel Transformations
-
-
-
-
-# Drop any new columns with NaN due to improper transformation
-cont_vars_df.replace([np.inf, -np.inf], np.nan, inplace=True)
-cont_vars_df.dropna(axis=1, inplace=True)
+# Impute or remove? (for now remove any columns with nan)
+cont_vars_df = x_data[x_data.columns[x_data.iloc[:, :].dtypes == 'float64']].dropna(axis=1)
 
 # Assume skewed if we can reject the null hypothesis with 95% certainty
 # Remove any skewed features after adding transformations
@@ -222,4 +103,10 @@ cont_vars_df = cont_vars_df.loc[:, cont_vars_df.apply(
     lambda x: skewtest(x)[1] > .05).values]
 
 # Combine datasets
-vars_df = pd.concat([cat_vars_df, cont_vars_df], axis=1)
+x_data = pd.concat([cat_vars_df, cont_vars_df], axis=1)
+
+
+
+
+
+
